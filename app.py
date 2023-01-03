49b6a3b85bd599980e5b145bda3b0a7c7c72cada
Denis Kaysarov <kaysik_it@mail.ru>
2023-01-03 22:00:31 +0500

    Hotfix

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import os
 from flask import Flask
-from flask_session import Session
+
+from config import SECRET_KEY, UPLOAD_FOLDER
 from flask_blog.views.view import view
+from flask_session import Session
 
 app = Flask(__name__, static_folder='flask_blog/static')
 app.register_blueprint(view, url_prefix="")
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config["SESSION_PERMANENT"] = False
-app.secret_key = os.getenv('SECRET_KEY')
+app.secret_key = SECRET_KEY
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 Session(app)
 
